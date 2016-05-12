ce9e5bb8fa3de0ea49027c59e43d1e79ad51909f
abennouna12 <aBennouna@MacBook-Pro-de-Abderrahmane.local>
2016-05-12 10:41:57 +0200

    DatabaseEdit1

--- src/java/Database/db.sql
+++ src/java/Database/db.sql
@@ -1,30 +1,46 @@
-ALTER TABLE produit DROP CONSTRAINT P_Prod_Sup0;
-ALTER TABLE produit DROP CONSTRAINT P_Haut_Sup0;
-ALTER TABLE produit DROP CONSTRAINT P_Long_Sup0;
-ALTER TABLE produit DROP CONSTRAINT P_NbEmpil_Sup0;
-ALTER TABLE produit DROP CONSTRAINT P_SetUp_N;
+ALTER TABLE type_produit DROP CONSTRAINT P_Prod_Sup0;
+ALTER TABLE type_produit DROP CONSTRAINT P_Haut_Sup0;
+ALTER TABLE type_produit DROP CONSTRAINT P_Long_Sup0;
+ALTER TABLE type_produit DROP CONSTRAINT P_NbEmpil_Sup0;
+ALTER TABLE type_produit DROP CONSTRAINT P_SetUp_N;
 
 ALTER TABLE commande DROP CONSTRAINT C_Stock_Sup0;
 ALTER TABLE commande DROP CONSTRAINT C_Envoi_Sup0;
 ALTER TABLE commande DROP CONSTRAINT C_Penalite_Sup0;
 
-ALTER TABLE production DROP CONSTRAINT P_NbLigne_N;
-
-ALTER TABLE box DROP CONSTRAINT B_Lbox_Sup0;
-ALTER TABLE box DROP CONSTRAINT B_Hbox_Sup0;
-ALTER TABLE box DROP CONSTRAINT B_Prix_Sup0;
-
 ALTER TABLE produit_commande DROP CONSTRAINT PC_Unites_N;
 ALTER TABLE produit_commande DROP CONSTRAINT PC_FK_Commande;
 ALTER TABLE produit_commande DROP CONSTRAINT PC_FK_Produit;
 
-DROP TABLE produit;
-DROP TABLE commande;
-DROP TABLE production;
-DROP TABLE box;
-DROP TABLE produit_commande;
+ALTER TABLE type_box DROP CONSTRAINT B_Lbox_Sup0;
+ALTER TABLE type_box DROP CONSTRAINT B_Hbox_Sup0;
+ALTER TABLE type_box DROP CONSTRAINT B_Prix_Sup0;
+ALTER TABLE box_achete DROP CONSTRAINT BA_FK_Commande;
+ALTER TABLE box_achete DROP CONSTRAINT BA_FK_Type_box;
 
-CREATE TABLE produit (
+ALTER TABLE pile DROP CONSTRAINT P_LongPile;
+ALTER TABLE pile DROP CONSTRAINT P_LargPile;
+ALTER TABLE pile DROP CONSTRAINT P_FK_BoxA;
+ALTER TABLE pile DROP CONSTRAINT P_FK_Prod;
+
+ALTER TABLE ligne_production DROP CONSTRAINT LP_NbLigne_N;
+ALTER TABLE ligne_production DROP CONSTRAINT LP_FK_Produit;
+
+ALTER TABLE produit DROP CONSTRAINT P_FK_P_C;
+ALTER TABLE produit DROP CONSTRAINT P_FK_box;
+
+DROP TABLE type_produit;
+DROP TABLE commande;
+DROP TABLE produit_commande;
+DROP TABLE type_box;
+DROP TABLE box_achete;
+DROP TABLE pile;
+DROP TABLE ligne_production;
+DROP TABLE produit;
+
+
+
+CREATE TABLE type_produit (
     id   VARCHAR(55) PRIMARY KEY,
     t_setup INTEGER,
     t_production INTEGER,
@@ -33,13 +49,6 @@ CREATE TABLE produit (
     nbEmpileMax INTEGER
 );
 
-ALTER TABLE produit ADD CONSTRAINT P_Prod_Sup0 CHECK (t_production > 0);
-ALTER TABLE produit ADD CONSTRAINT P_Haut_Sup0 CHECK (hauteur > 0);
-ALTER TABLE produit ADD CONSTRAINT P_Long_Sup0 CHECK (longueur > 0);
-ALTER TABLE produit ADD CONSTRAINT P_NbEmpil_Sup0 CHECK (nbEmpileMax > 0);
-ALTER TABLE produit ADD CONSTRAINT P_SetUp_N CHECK (nbEmpileMax >= 0);
-
-
 CREATE TABLE commande (
     id   VARCHAR(55) PRIMARY KEY,
     stockMin INTEGER,
@@ -47,36 +56,77 @@ CREATE TABLE commande (
     penalite FLOAT
 );
 
-
-ALTER TABLE commande ADD CONSTRAINT C_Stock_Sup0 CHECK (stockMin > 0);
-ALTER TABLE commande ADD CONSTRAINT C_Envoi_Sup0 CHECK (dEnvoiPrevue > 0);
-ALTER TABLE commande ADD CONSTRAINT C_Penalite_Sup0 CHECK (penalite > 0);
-
-CREATE TABLE production (
-    nbLignes INTEGER
+CREATE TABLE produit_commande (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_commande VARCHAR(55) NOT NULL,
+    id_type_produit VARCHAR(55)NOT NULL,
+    nb_unites INTEGER
 );
 
-ALTER TABLE production ADD CONSTRAINT P_NbLigne_N CHECK (nbLignes >= 0);
-
-CREATE TABLE box (
+CREATE TABLE type_box (
     id   VARCHAR(55) PRIMARY KEY,
     Lbox INTEGER NOT NULL,
     Hbox INTEGER NOT NULL,
     prixBox FLOAT NOT NULL
 );
 
-
-ALTER TABLE box ADD CONSTRAINT B_Lbox_Sup0 CHECK (Lbox > 0);
-ALTER TABLE box ADD CONSTRAINT B_Hbox_Sup0 CHECK (Hbox > 0);
-ALTER TABLE box ADD CONSTRAINT B_Prix_Sup0 CHECK (prixBox >= 0);
-
-CREATE TABLE produit_commande (
+CREATE TABLE box_achete (
     id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    id_commande VARCHAR(55) NOT NULL,
-    id_produit VARCHAR(55)NOT NULL,
-    nb_unites INTEGER
+    id_type_box VARCHAR(55) NOT NULL,
+    id_commande VARCHAR(55) NOT NULL
 );
+
+CREATE TABLE produit (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_produit_commande INTEGER NOT NULL,
+    id_box INTEGER NOT NULL,
+    date_arrivee_box Date
+);
+
+CREATE TABLE pile (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_box_achete INTEGER,
+    longueur_pile INTEGER,
+    largeur_pile INTEGER,
+    id_produit INTEGER
+);
+
+CREATE TABLE ligne_production (
+    nbLignes INTEGER,
+    date_dispo Date,
+    id_produit INTEGER
+);
+
+
+
+ALTER TABLE type_produit ADD CONSTRAINT P_Prod_Sup0 CHECK (t_production > 0);
+ALTER TABLE type_produit ADD CONSTRAINT P_Haut_Sup0 CHECK (hauteur > 0);
+ALTER TABLE type_produit ADD CONSTRAINT P_Long_Sup0 CHECK (longueur > 0);
+ALTER TABLE type_produit ADD CONSTRAINT P_NbEmpil_Sup0 CHECK (nbEmpileMax > 0);
+ALTER TABLE type_produit ADD CONSTRAINT P_SetUp_N CHECK (nbEmpileMax >= 0);
+
+ALTER TABLE type_box ADD CONSTRAINT B_Lbox_Sup0 CHECK (Lbox > 0);
+ALTER TABLE type_box ADD CONSTRAINT B_Hbox_Sup0 CHECK (Hbox > 0);
+ALTER TABLE type_box ADD CONSTRAINT B_Prix_Sup0 CHECK (prixBox >= 0);
+
+ALTER TABLE commande ADD CONSTRAINT C_Stock_Sup0 CHECK (stockMin > 0);
+ALTER TABLE commande ADD CONSTRAINT C_Envoi_Sup0 CHECK (dEnvoiPrevue > 0);
+ALTER TABLE commande ADD CONSTRAINT C_Penalite_Sup0 CHECK (penalite > 0);
 
 ALTER TABLE produit_commande ADD CONSTRAINT PC_Unites_N CHECK (nb_unites >= 0);
 ALTER TABLE produit_commande ADD CONSTRAINT PC_FK_Commande FOREIGN KEY(id_commande) REFERENCES commande(id);
-ALTER TABLE produit_commande ADD CONSTRAINT PC_FK_Produit FOREIGN KEY(id_produit) REFERENCES produit(id);
+ALTER TABLE produit_commande ADD CONSTRAINT PC_FK_Produit FOREIGN KEY(id_type_produit) REFERENCES type_produit(id);
+
+ALTER TABLE box_achete ADD CONSTRAINT BA_FK_Commande FOREIGN KEY(id_commande) REFERENCES commande(id);
+ALTER TABLE box_achete ADD CONSTRAINT BA_FK_Type_box FOREIGN KEY(id_type_box) REFERENCES type_box(id);
+
+ALTER TABLE produit ADD CONSTRAINT P_FK_P_C FOREIGN KEY(id_produit_commande) REFERENCES produit_commande(id);
+ALTER TABLE produit ADD CONSTRAINT P_FK_box FOREIGN KEY(id_box) REFERENCES box_achete(id);
+
+ALTER TABLE pile ADD CONSTRAINT P_LongPile CHECK (longueur_pile >= 0);
+ALTER TABLE pile ADD CONSTRAINT P_LargPile CHECK (largeur_pile >= 0);
+ALTER TABLE pile ADD CONSTRAINT P_FK_BoxA FOREIGN KEY(id_box_achete) REFERENCES box_achete(id);
+ALTER TABLE pile ADD CONSTRAINT P_FK_Prod FOREIGN KEY(id_produit) REFERENCES produit(id);
+
+ALTER TABLE ligne_production ADD CONSTRAINT LP_NbLigne_N CHECK (nbLignes >= 0);
+ALTER TABLE ligne_production ADD CONSTRAINT LP_FK_Produit FOREIGN KEY(id_produit) REFERENCES produit(id);
