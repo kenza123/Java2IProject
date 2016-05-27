01a067b7428f50281ed6010a957c31b2aa2ce0f8
kenza123 <kham8kenza@gmail.com>
2016-05-27 15:32:59 +0200

    set up stats page + redesigned index page

--- src/java/dao/BoxAcheteDao.java
+++ src/java/dao/BoxAcheteDao.java
@@ -14,5 +14,6 @@ import model.TypeBox;
  */
 public interface BoxAcheteDao extends Dao<BoxAchete> {
     public int countBoxesByTypeBox(TypeBox typeBox);
+    public int countBoxesById(String idTypeBox);
     
 }
