18cbce3664c96c999f5005c33aa831a314397723
abennouna12 <a.bennouna.a@gmail.com>
2016-06-04 03:32:00 +0200

    Adding lines to order page
    - Adding lines to order page

--- src/java/controlleur/CommandeControl.java
+++ src/java/controlleur/CommandeControl.java
@@ -5,14 +5,20 @@
  */
 package controlleur;
 
+import dao.DaoFactory;
+import dao.JpaDaoFactory;
+import dao.JpaDaoProduitCommande;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import model.BoxAchete;
 import model.Commande;
+import model.CommandeBox;
 import model.Produit;
+import model.ProduitCommande;
 
 /**
  *
@@ -50,20 +56,50 @@ public class CommandeControl implements Serializable {
     public List<Produit> getProduitsAchetes(BoxAchete boxAchete) {
         List<Produit> produits = new ArrayList();
         boxAchete.getPileCollection().stream().forEach((pile) -> {
-        produits.addAll(pile.getProduitCollection());
+            pile.getProduitCollection().stream().forEach((produit) -> {
+                if(!produits.contains(produit)) {
+                    produits.add(produit);
+                }
+            });
         });
         return produits;
     }
     
-    
-    
     public String showPage(Commande commande) {
         this.commande = commande;
         this.boxAchetes = new ArrayList<>();
+        Collection<BoxAchete> boxes = new ArrayList<>();
         commande.getCommandeBoxCollection().stream().forEach((commandeBox)->{
-            this.boxAchetes.add(commandeBox.getIdBoxAchete());
+            System.out.println(commandeBox.getIdBoxAchete().getIdTypeBox().getId() + "_" + commandeBox.getIdBoxAchete().getNumBox());
+            if(!boxes.contains(commandeBox.getIdBoxAchete())) {
+                boxes.add(commandeBox.getIdBoxAchete()); 
+            }
         });
+        this.boxAchetes.addAll(boxes);
         return "commande";
+    }
+    
+    public String getDebutProd(BoxAchete boxAchete) {
+        return "check commandeControl.getDebutProd()";
+    }
+    
+    public List<Produit> getProducts() {
+        List<Produit> produits = new ArrayList<>();
+        JpaDaoFactory jpaDaoFactory = (JpaDaoFactory) DaoFactory.getDaoFactory(DaoFactory.PersistenceType.JPA);
+        JpaDaoProduitCommande jpaDaoProduitCommande  = jpaDaoFactory.getProduitCommandeDao();
+        for(ProduitCommande produitCommande :jpaDaoProduitCommande.findProductsOfCommande(commande)) {
+            for(Produit produit : produitCommande.getProduitCollection()) {
+                if(!produits.contains(produit)) {
+                    produits.add(produit);
+                }
+            }
+        }
+        return produits;
+    }
+    
+    
+    public Boolean productExist(Produit produit) {
+        return getProducts().contains(produit);
     }
     
 }
